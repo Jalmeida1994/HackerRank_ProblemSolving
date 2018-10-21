5aff2544c54d649308bb2cf905213b7560796440
João Carlos Ferra de Almeida <jcfd.almeida@campus.fct.unl.pt>
2018-10-21 18:20:53 +0100

    1 more exercise (FAILED 4 TESTS)
    got to review it

---
+++ Time Conversion.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+
+// This solution passed all except 4 test cases, got to review this one
+
+public class Solution {
+
+    /*
+     * Complete the timeConversion function below.
+     */
+    static String timeConversion(String s) {
+        if(s.indexOf('P') >= 0){
+            Integer n = Integer.parseInt(s.substring(0, 2));
+            s = removeHour(s);
+            n += 12;
+            String hour = Integer.toString(n);
+            s = hour + s;
+        }
+        else if (s.indexOf('A') >= 0 && s.substring(0, 2).equals("12")){
+          "00".substring(2);
+
+        }
+        return removeHourFormat(s);
+    }
+
+    private static String removeHourFormat(String str) {
+        return str.substring(0, str.length() - 2);
+    }
+
+    private static String removeHour(String str) {
+        return str.substring(2, str.length());
+    }
+
+    private static final Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = scan.nextLine();
+
+        String result = timeConversion(s);
+
+        bw.write(result);
+        bw.newLine();
+
+        bw.close();
+    }
+}
