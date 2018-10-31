9da0ca8f606a5e033cbb97d93cd8d38ae45ba436
João Carlos Ferra de Almeida <jcfd.almeida@campus.fct.unl.pt>
2018-10-31 14:22:05 +0000

    Another exercise added
    all tests passed

---
+++ Migratory Birds.java
@@ -0,0 +1,65 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class Solution {
+
+    // Complete the migratoryBirds function below.
+    static int migratoryBirds(List<Integer> arr) {
+        // Insert all elements in hash
+        Map<Integer, Integer> hp = new HashMap<Integer, Integer>();
+
+        for (int i = 0; i < arr.size(); i++) {
+            int key = arr.get(i);
+            if (hp.containsKey(key)) {
+                int freq = hp.get(key);
+                freq++;
+                hp.put(key, freq);
+            } else {
+                hp.put(key, 1);
+            }
+        }
+
+        // find max frequency.
+        int max_count = 0, res = -1;
+
+        for (Entry<Integer, Integer> val : hp.entrySet()) {
+            if (max_count < val.getValue()) {
+                res = val.getKey();
+                max_count = val.getValue();
+            }
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int arrCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = migratoryBirds(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
